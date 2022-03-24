6ad2e4bafc9e8b56186713c8aaece06b8a436bf6
katarynaa <kasia.adamczyk2002@icloud.com>
2022-03-24 21:25:06 +0100

    pierwszy update

---
+++ bibliotekacal/cal/__init__.py
@@ -0,0 +1,8 @@
+def add_number(n1, n2):
+    return n1+n2
+def subtract_number(n1, n2):
+    return n1-n2
+def divide_number(n1, n2):
+    return n1/n2
+def multiply_number(n1, n2):
+    return n1*n2
---
+++ bibliotekacal/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='calculator',
+    version='0.1',
+    description='this is a simple calculator that is operating on two numbers',
+    url='',
+    author='kasia',
+    author_email='kadamczyk15@st.swps.edu.pl',
+    license='unlicense',
+    package='cal',
+    zip_safe=False
+
+)
